68a02aa4e89c4ec806fef450eb374f5621ae7da2
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2026-02-25 13:33:39 +0530

    All code update of panel andbase url updated

--- src/helper/Helper.js
+++ src/helper/Helper.js
@@ -1,2 +1,3 @@
 // export const baseUrl = 'http://82.25.109.68:5010'
-export const baseUrl = 'https://www.gtright.in'
+export const baseUrl = 'https://panel.gtright.in'
+// export const baseUrl = 'http://localhost:5001'
